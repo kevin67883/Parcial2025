531c59cce97026d74e84aaeb00d389c25a47da2a
Kevin Manrique Campos Granados <USSS027225@ugb.edu.sv>
2025-09-04 09:20:12 -0600

    Conversion de are parcial

---
+++ Areas/Areas/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Areas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblRespuesta = new System.Windows.Forms.Label();
+            this.cboDe = new System.Windows.Forms.ComboBox();
+            this.cboA = new System.Windows.Forms.ComboBox();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(337, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "VALOR:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(201, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "DE:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(446, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "A:";
+            // 
+            // lblRespuesta
+            // 
+            this.lblRespuesta.AutoSize = true;
+            this.lblRespuesta.Location = new System.Drawing.Point(320, 349);
+            this.lblRespuesta.Name = "lblRespuesta";
+            this.lblRespuesta.Size = new System.Drawing.Size(69, 13);
+            this.lblRespuesta.TabIndex = 2;
+            this.lblRespuesta.Text = "RESULADO:";
+            // 
+            // cboDe
+            // 
+            this.cboDe.FormattingEnabled = true;
+            this.cboDe.Items.AddRange(new object[] {
+            "Pie Cuadrado",
+            "",
+            "Vara Cuadrada",
+            "",
+            "Yarda Cuadrada",
+            "",
+            " Metro Cuadrado",
+            "",
+            " Tareas",
+            "",
+            " Manzana",
+            "",
+            "Hectárea"});
+            this.cboDe.Location = new System.Drawing.Point(271, 148);
+            this.cboDe.Name = "cboDe";
+            this.cboDe.Size = new System.Drawing.Size(121, 21);
+            this.cboDe.TabIndex = 3;
+            // 
+            // cboA
+            // 
+            this.cboA.FormattingEnabled = true;
+            this.cboA.Items.AddRange(new object[] {
+            "Pie Cuadrado",
+            "",
+            "Vara Cuadrada",
+            "",
+            "Yarda Cuadrada",
+            "",
+            " Metro Cuadrado",
+            "",
+            " Tareas",
+            "",
+            " Manzana",
+            "",
+            "Hectárea"});
+            this.cboA.Location = new System.Drawing.Point(503, 151);
+            this.cboA.Name = "cboA";
+            this.cboA.Size = new System.Drawing.Size(121, 21);
+            this.cboA.TabIndex = 4;
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(458, 89);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(100, 20);
+            this.txtValor.TabIndex = 5;
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Location = new System.Drawing.Point(425, 368);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(147, 43);
+            this.btnConvertir.TabIndex = 6;
+            this.btnConvertir.Text = "CONVERTIR:";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.cboA);
+            this.Controls.Add(this.cboDe);
+            this.Controls.Add(this.lblRespuesta);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblRespuesta;
+        private System.Windows.Forms.ComboBox cboDe;
+        private System.Windows.Forms.ComboBox cboA;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Button btnConvertir;
+    }
+}
+
---
+++ Areas/Areas/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Areas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Unidades y equivalencias en metros cuadrados
+        string[] unidades = { "Pie Cuadrado", "Vara Cuadrada", "Yarda Cuadrada", "Metro Cuadrado", "Tareas", "Manzana", "Hectárea" };
+        double[] valores = { 0.092903, 0.705012, 0.836127, 1.0, 628.86, 6970, 10000 };
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtValor.Text))
+                {
+                    lblRespuesta.Text = "Ingrese un valor";
+                    return;
+                }
+                if (cboDe.SelectedIndex == -1 || cboA.SelectedIndex == -1)
+                {
+                    lblRespuesta.Text = "Seleccione ambas unidades";
+                    return;
+                }
+
+                // Usar TryParse para evitar excepciones en números mal escritos
+                if (!double.TryParse(txtValor.Text, out double valor))
+                {
+                    lblRespuesta.Text = "Error: ingrese solo números válidos";
+                    return;
+                }
+
+                int de = cboDe.SelectedIndex;
+                int a = cboA.SelectedIndex;
+
+                double resultado = valor * valores[de] / valores[a];
+                lblRespuesta.Text = $"RESPUESTA: {resultado:N4}";
+            }
+            catch (Exception)
+            {
+                lblRespuesta.Text = "Error inesperado";
+            }
+        }
+    }
+}
